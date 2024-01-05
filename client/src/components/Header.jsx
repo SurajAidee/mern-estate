7fdeea3f4d46390c85d77d0c9af0dc35b378a4c1
SurajAidee <114228979+SurajAidee@users.noreply.github.com>
2024-01-05 16:58:36 +0545

    add onChange and onSubmit unctionality to the search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,9 +5,8 @@ import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  const { searchTerm, setSearchTerm } = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
@@ -24,17 +23,17 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className="bg-slate-200">
+    <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Mern</span>
+            <span className="text-slate-500">Sahand</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
         <form
           onSubmit={handleSubmit}
-          className="bg-slate-100 p-3 rounded-lg flex item-center"
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
         >
           <input
             type="text"
@@ -49,24 +48,24 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-xlate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-xlate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
               About
             </li>
           </Link>
           <Link to="/profile">
             {currentUser ? (
               <img
-                className="rounded-full  h-7 w-7 object-cover "
+                className="rounded-full h-7 w-7 object-cover"
                 src={currentUser.avatar}
                 alt="profile"
               />
             ) : (
-              <li className="text-xlate-700 hover:underline">Sign in</li>
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
             )}
           </Link>
         </ul>
